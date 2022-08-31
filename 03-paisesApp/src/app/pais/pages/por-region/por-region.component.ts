8b2969e8bbdde3c3f5ee26b41e9f7ef4cb9d7de3
Iker Abadia <iker.abadia@bosonit.com>
2022-08-31 18:38:32 +0200

    123 - sin empezar
    123 - sin empezar

--- 03-paisesApp/src/app/pais/pages/por-region/por-region.component.ts
+++ 03-paisesApp/src/app/pais/pages/por-region/por-region.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-por-region',
   templateUrl: './por-region.component.html',
   styleUrls: ['./por-region.component.css']
 })
-export class PorRegionComponent implements OnInit {
+export class PorRegionComponent {
+
+  regiones: string[] = ['africa', 'americas', 'asia', 'europe', 'oceania'];
+  regionActiva: string = '';
 
   constructor() { }
 
-  ngOnInit(): void {
+  activarRegion(region: string){
+    this.regionActiva = region;
+
+    //TODO: hacer el llamado al servicio
   }
+
+  
 
 }
--- 03-paisesApp/src/app/pais/pages/ver-pais/ver-pais.component.ts
+++ 03-paisesApp/src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { PaisService } from '../../services/pais.service';
+import { switchMap, tap } from "rxjs/operators";
+import { Country } from '../../interfaces/pais.interfaces';
 
 @Component({
   selector: 'app-ver-pais',
@@ -9,6 +11,8 @@ import { PaisService } from '../../services/pais.service';
 })
 export class VerPaisComponent implements OnInit {
 
+  pais!: Country;
+
   constructor(
     private activatedRoute: ActivatedRoute,
     private paisService: PaisService
@@ -16,11 +20,19 @@ export class VerPaisComponent implements OnInit {
 
   ngOnInit(): void {
 
-    this.activatedRoute.params.subscribe( params => {
+    this.activatedRoute.params
+      .pipe(
+        switchMap((params) => this.paisService.getPaisPorAlpha( params["id"])),
+        tap( console.log )
+      ).subscribe( pais => {
+        console.log("stop");
+        this.pais = pais[0];
+      });
+    /* this.activatedRoute.params.subscribe( params => {
       this.paisService.getPaisPorAlpha(params['id']).subscribe( pais => {
         console.log(pais);
       })
-    })
+    }) */
 
 
 
